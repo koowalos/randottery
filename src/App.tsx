47e971b8d2906c13a442e9a83679682a087ae44f
Pawel Kownacki <koowal@gmail.com>
2020-06-11 15:07:23 +0100

    react-router

--- src/App.tsx
+++ src/App.tsx
@@ -10,20 +10,43 @@ import Lottery from './Components/Pages/Lottery';
 import Error404 from './Components/Pages/Error404';
 import history from './history';
 
+import { Layout, Menu, Breadcrumb } from 'antd';
+const { Header, Content, Footer } = Layout;
+
 function App() {
   return (
     <div className="App">
-      <Router history={history}>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/landing" component={Landing} />
-          <Route exact path="/join" component={Join} />
-          <Route exact path="/join/:id" component={JoinDetails} />
-          <Route exact path="/new" component={New} />
-          <Route path="/lottery/:id" component={Lottery} />
-          <Route component={Error404} />
-        </Switch>
-      </Router>
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['1']}
+            style={{ textAlign: 'right' }}
+          >
+            <Menu.Item key="1">Sign in</Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <div className="site-layout-content">
+            <Router history={history}>
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/landing" component={Landing} />
+                <Route exact path="/join" component={Join} />
+                <Route exact path="/join/:id" component={JoinDetails} />
+                <Route exact path="/new" component={New} />
+                <Route path="/lottery/:id" component={Lottery} />
+                <Route component={Error404} />
+              </Switch>
+            </Router>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
     </div>
   );
 }
--- src/Components/Pages/Home/Home.tsx
+++ src/Components/Pages/Home/Home.tsx
@@ -1,8 +1,51 @@
 import React from 'react';
+import { Tabs, Row, Col, Button } from 'antd';
+
+const { TabPane } = Tabs;
 interface HomeProps {
-  someProp?: any;
+  history: {
+    push: Function;
+  };
 }
+function callback(key) {
+  console.log(key);
+}
+
 const Home: React.FC<HomeProps> = (props) => {
-  return <div>Home Functional Component</div>;
+  console.log(props);
+  return (
+    <div>
+      <Row>
+        <Col span={12}>
+          <h1>Participate</h1>
+          <Button
+            onClick={() => {
+              props.history.push('/join');
+            }}
+          >
+            JOIN
+          </Button>
+          <Tabs defaultActiveKey="1" onChange={callback}>
+            <TabPane tab="In progress" key="1">
+              Content of Tab Pane 1
+            </TabPane>
+            <TabPane tab="Ended" key="2">
+              Content of Tab Pane 2
+            </TabPane>
+          </Tabs>
+        </Col>
+        <Col span={12}>
+          <Tabs defaultActiveKey="1" onChange={callback}>
+            <TabPane tab="In progress" key="1">
+              Content of Tab Pane 1
+            </TabPane>
+            <TabPane tab="Ended" key="2">
+              Content of Tab Pane 2
+            </TabPane>
+          </Tabs>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 export default Home;
