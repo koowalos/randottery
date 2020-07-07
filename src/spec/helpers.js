a41a5ab993d5cc18b485329e1bea573136ff6b11
Kamil Albrycht <ponciusz@gmail.com>
2020-07-07 21:49:11 +0100

    update security rules

--- src/spec/helpers.js
+++ src/spec/helpers.js
@@ -2,19 +2,24 @@ const firebase = require('@firebase/testing');
 const fs = require('fs');
 
 module.exports.setup = async (auth, data) => {
-  console.log('------------------- SETUP -------------------');
   const projectId = `rules-spec-${Date.now()}`;
+  // const projectId = `randomizer-ae71a`;
   const app = await firebase.initializeTestApp({
     projectId,
     auth,
   });
 
+  const adminApp = await firebase.initializeAdminApp({
+    projectId,
+  });
+
+  const adminDb = adminApp.firestore();
   const db = app.firestore();
 
   // Write mock documents before rules
   if (data) {
     for (const key in data) {
-      const ref = db.doc(key);
+      const ref = adminDb.doc(key);
       await ref.set(data[key]);
     }
   }
--- src/spec/rules.spec.js
+++ src/spec/rules.spec.js
@@ -1,41 +1,17 @@
+import { mockData, mockUser } from './mocks';
 const { setup, teardown } = require('./helpers');
-const { assertFails, assertSucceeds } = require('@firebase/testing');
-
-const mockUser = {
-  uid: 'ponciusz',
-  email: 'ponciusz@gmail.com',
-};
-
-const mockData = {
-  'users/ponciusz': {
-    displayName: 'My Name',
-    email: 'ponciusz@gmail.com',
-    photoURL: null,
-  },
-  'lotteries/ponciusz1': {
-    endDate: '2020-07-11 23:15:41',
-    endWhenFull: true,
-    maxParticipants: 100,
-    name: 'loteria',
-    numberOfWinners: 1,
-    owner: 'ponciusz',
-    participants: ['ponciusz'],
-    prize: 'banan',
-    status: 'active',
-  },
-};
 
 describe('Database rules', () => {
   let db;
   let ref;
   let refLotteries;
-  console.log('describe');
+  let refUsers;
   // Applies only to tests in this describe block
   beforeAll(async () => {
-    db = await setup();
-    // All paths are secure by default
+    db = await setup(mockUser, mockData);
     ref = db.collection('some-nonexistent-collection');
     refLotteries = db.collection('lotteries');
+    refUsers = db.collection('users');
   });
 
   afterAll(async () => {
@@ -43,10 +19,112 @@ describe('Database rules', () => {
     await teardown();
   });
 
-  // test('fail when reading/writing an unauthorized collection', async () => {
-  //   console.log({ refLotteries });
-  //   // Custom Matchers
-  //   await expect(ref.get()).toAllow();
+  test('DENY when reading/writing an unauthorized collection', async () => {
+    await expect(ref.get()).toDeny();
+  });
+
+  test('DENY when read users collection', async () => {
+    await expect(refUsers.get()).toDeny();
+  });
+
+  test('ALLOW when read current user', async () => {
+    await expect(refUsers.doc('ponciusz')).toAllow();
+  });
+
+  test('DENY when read other user profile', async () => {
+    await expect(refUsers.doc('kowal').get()).toDeny();
+  });
+
+  test('ALLOW CREATE Lottery', async () => {
+    await expect(
+      refLotteries.doc('ponciusz_new').set({
+        endDate: '2020-07-11 23:15:41',
+        endWhenFull: true,
+        maxParticipants: 100,
+        name: 'loteria',
+        numberOfWinners: 1,
+        owner: 'ponciusz',
+        participants: [],
+        prize: 'banan',
+        status: 'active',
+      })
+    ).toAllow();
+  });
+
+  test('ALLOW DELETE own empty Lottery', async () => {
+    await expect(refLotteries.doc('ponciusz_new').delete()).toAllow();
+  });
+
+  test('DENY CREATE lottery with predefined participants', async () => {
+    await expect(
+      refLotteries.doc('ponciusz3').set({
+        endDate: '2020-07-11 23:15:41',
+        endWhenFull: true,
+        maxParticipants: 100,
+        name: 'loteria',
+        numberOfWinners: 1,
+        owner: 'ponciusz',
+        participants: ['someUser'],
+        prize: 'banan',
+        status: 'active',
+      })
+    ).toDeny();
+  });
+
+  test('DENY CREATE lottery as somebody else', async () => {
+    await expect(
+      refLotteries.doc('ponciusz_new2').set({
+        endDate: '2020-07-11 23:15:41',
+        endWhenFull: true,
+        maxParticipants: 100,
+        name: 'loteria',
+        numberOfWinners: 1,
+        owner: 'kowal',
+        participants: [],
+        prize: 'banan',
+        status: 'active',
+      })
+    ).toDeny();
+  });
+
+  test('DENY JOIN own lottery', async () => {
+    await expect(
+      refLotteries.doc('ponciusz_a').update({
+        participants: ['koowal', 'kula', 'ponciusz'],
+      })
+    ).toDeny();
+  });
+
+  test('ALLOW JOIN lottery', async () => {
+    await expect(
+      refLotteries.doc('koowal_b').update({
+        participants: ['kula', 'ponciusz'],
+      })
+    ).toAllow();
+  });
+
+  test('ALLOW LEAVE lottery', async () => {
+    await expect(
+      refLotteries.doc('koowal_b').update({
+        participants: ['kula'],
+      })
+    ).toAllow();
+  });
+
+  test('DENY JOIN ended lottery', async () => {
+    await expect(
+      refLotteries.doc('kowal_c').update({
+        participants: ['kula', 'ponciusz'],
+      })
+    ).toDeny();
+  });
+
+  // test('Read all lotteries', async () => {
+  //   console.log('------------------- AFTER -------------------');
+  //   const data = await refLotteries.get();
+  //   data.forEach(function (doc) {
+  //     console.log(doc.id, ' => ', doc.data());
+  //   });
   // });
 
   // test('Read Lottery', async () => {
@@ -54,31 +132,19 @@ describe('Database rules', () => {
   //   await expect(refLotteries.get()).toAllow();
   // });
 
-  test('Add Lottery', async () => {
-    await expect(
-      refLotteries.doc('xxx').set({
-        endDate: '2020-07-11 23:15:41',
-        endWhenFull: true,
-        maxParticipants: 100,
-        name: 'loteria',
-        numberOfWinners: 1,
-        owner: 'ponciusz',
-        participants: ['ponciusz'],
-        prize: 'banan',
-        status: 'active',
-      })
-    ).toAllow();
-  });
-
-  // test('Add Lottery', async () => {
-  //   const data = await refLotteries.doc('xxx').get();
-  //   console.log(data.data());
+  // test('Read Lotteries', async () => {
+  //   const data = await refLotteries.get();
+  //   console.log('------------------- BEFORE -------------------');
+  //   data.forEach(function (doc) {
+  //     console.log('before:', doc.id, ' => ', doc.data());
+  //   });
   // });
 
-  test('Add Lottery', async () => {
-    const data = await refLotteries.get();
-    data.forEach(function (doc) {
-      console.log(doc.id, ' => ', doc.data());
-    });
-  });
+  // test('Read Users', async () => {
+  //   const data = await refUsers.get();
+  //   console.log('------------------- USERS -------------------');
+  //   data.forEach(function (doc) {
+  //     console.log('before:', doc.id, ' => ', doc.data());
+  //   });
+  // });
 });
